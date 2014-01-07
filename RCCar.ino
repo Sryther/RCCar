1be52d8bd765fd44399971ae6930c093f14837b6
Paul Rey <paul.rey@ingesup.com>
2014-01-07 14:56:34 +0100

    Ajout du code minimal

--- RCCar.ino
+++ RCCar.ino
@@ -1,3 +1,11 @@
 #include <nmea.h>
 
+void setup()          //fonction d'initialisation de la carte
+{
+                      //contenu de l'initialisation
+}
 
+void loop()           //fonction principale, elle se répète (s’exécute) à l'infini
+{
+                      //contenu de votre programme
+}
